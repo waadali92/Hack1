6cc92b56daeb015231a6d323c81b2591dcdc9761
Waad Ali <waadali@Waads-MacBook-Pro.local>
2018-04-04 08:34:31 +0300

    Initial Commit

---
+++ ElmHackathon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ElmHackathon
+//
+//  Created by Waad Ali on 7/18/1439 AH.
+//  Copyright © 1439 Waad Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ElmHackathonTests/ElmHackathonTests.swift
@@ -0,0 +1,36 @@
+//
+//  ElmHackathonTests.swift
+//  ElmHackathonTests
+//
+//  Created by Waad Ali on 7/18/1439 AH.
+//  Copyright © 1439 Waad Ali. All rights reserved.
+//
+
+import XCTest
+@testable import ElmHackathon
+
+class ElmHackathonTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
